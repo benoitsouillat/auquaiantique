29a6bd9900e526d2ee63dd8c296352b5f5c13fbd
Benoit Souillat <tido.squat@gmail.com>
2023-03-09 11:03:32 +0100

    create logic of click on gallery

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({label}) => {
   return (
-    <button className='button'>
+    <button className='button' type='button'>
         {label}
     </button>
   )
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from "./Button";
 
 const Gallery = () => {
@@ -6,59 +6,88 @@ const Gallery = () => {
     const foodImg = [
         {
             url: "/img/bavette_echalote.jpg",
-            description: "Bavette aux oignons et Pommes de Terres Sautées"
+            description: "Bavette aux oignons et Pommes de Terres Sautées",
+            id: "bavette"
         },
         {
             url: "/img/boulettes.jpeg",
-            description: "Boulette de Viande Hachée"
+            description: "Boulette de Viande Hachée",
+            id: "boulette"
         },
         {
             url: "/img/breton.jpg",
-            description: "Une spécialité Bretonne"
+            description: "Une spécialité Bretonne",
+            id: "breton"
         },
         {
             url: "/img/farcies.jpg",
-            description: "Tomate farcies maison"
+            description: "Tomate farcies maison",
+            id: "farcies"
         },
         {
             url: "/img/toquet.jpg",
-            description: "desc"
+            description: "desc",
+            id: "toquet"
         },
         {
             url: "/img/spaghetti.jpg",
-            description: "Un plat de spaghetti"
+            description: "Un plat de spaghetti",
+            id: "spaghetti"
         },
         {
             url: "/img/pizza.jpg",
-            description: "Une Pizza faite maison"
+            description: "Une Pizza faite maison",
+            id: "pizza"
         },
         {
             url: "/img/presse_ail_tomate.jpg",
-            description: "Presse ail et tomate"
+            description: "Presse ail et tomate",
+            id: "presse"
         },
         {
             url: "/img/Roti_Gastro.jpg",
-            description: "La spécialité du chef"
+            description: "La spécialité du chef",
+            id: "roti"
         }
     ]
+    let [selectImg, setSelectImg] = useState(
+        {
+            url: 'img/bavette_echalote.jpg',
+            description: "Une Bavette aux échalotes"
+    });
+
+
+    const setImgShow = (id) => {
+        foodImg.map((pic) => {
+            if (pic.id === id) {
+                setSelectImg(pic);
+            }
+        });
+
+    };
+    const handleClickImg = (e) => {
+        setImgShow(e.target.id);
+    };
+
     return (
         <section className='gallery-section'>
             <h2>La Galerie</h2>
             <div className="gallery">
                 <div className='grid-gallery'>
                     {
-                        foodImg.map(pic => (
-                            <div>
-                                <img src={pic.url} alt={pic.description} />
+                        foodImg.map((pic, i) => (
+                            <div key={i} onClick={handleClickImg}>
+                                <img id={pic.id} src={pic.url} alt={pic.description} />
                             </div>
                         ))}
                 </div>
-
-
                 <Button label="Réserver une table" />
-
             </div>
             <div className='selected-image-container'>
+                <figure>
+                    <img src={selectImg.url} alt={selectImg.description} />
+                    <figcaption>{selectImg.description}</figcaption>
+                </figure>
 
             </div>
 
